33c30532d41fdd89524bc80e5b59155c11c76e9d
bitnara0156 <142308644+bitnara0156@users.noreply.github.com>
2023-10-31 13:46:13 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="carousel-wrapper">
+    <client-only>
+      <Slider :options="options">
+        <SliderItem
+          v-for="i in 5"
+          :key="i"
+          class="img-wrapper"
+          style="width: 33.3%;"
+        >
+          <img :src="`./${i}-200x100.jpg`" />
+        </SliderItem>
+      </Slider>
+    </client-only>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      options: {
+        slidesToScroll: 3,
+        loop: true,
+        pagination: false
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.carousel-wrapper {
+  padding: 40px;
+  height: 180px;
+}
+.img-wrapper img {
+  margin: auto;
+  width: 200px;
+  height: 100px;
+  background-image: linear-gradient(gray 100%, transparent 0);
+}
+</style>
